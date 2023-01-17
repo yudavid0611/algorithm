c65513a193f1991db1d1d6bf34ef75978d4c3a95
YU DAVID <yudavid0611@gmail.com>
2023-01-17 18:36:35 +0900

    0117

--- BOJ/16928/16928_solution.py
+++ BOJ/16928/16928_solution.py
@@ -6,53 +6,61 @@ from collections import defaultdict
 
 N, M = map(int, input().split())
 dp = [0] * 101
-dp[1] = 1
+
 ladder = defaultdict(int)
 snake = defaultdict(int)
 
-# 사다리
+# 사다리: 시작점-key, 도착점-value
 for _ in range(N):
     s, e = map(int, input().split())
     dp[s] = 100
     ladder[s] = e
 
-# 뱀
+# 뱀: 시작점-key, 도착점-value
 for _ in range(M):
     s, e = map(int, input().split())
     dp[s] = 100
     snake[s] = e
 
+# 키들만 따로 저장
 ladder_start = list(ladder.keys())
 snake_start = list(snake.keys())
 
-idx = 2
-
+idx = 1
 while idx != 101:    
-    # idx에서 1~6칸 전의 값 중 최소값
-    turn = min(dp[idx-6:idx]) + 1 if idx > 7 else min(dp[1:idx]) + 1
+    # idx 칸에서 1~6칸 전의 값 중 최소값
+    turn = min(dp[idx-6:idx]) + 1 if idx > 7 else min(dp[:idx]) + 1
     
+    # 이미 idx 칸에 값이 존재할 경우
     if dp[idx] and dp[idx] != 100:
         turn = min(turn, dp[idx])
     
     # idx 칸이 사다리 시작점일 경우
     if idx in ladder_start:
+        # 이미 사다리 도착점에 값이 있을 경우
         if dp[ladder[idx]]:
+            # 신규 값이 더 작을 경우 업데이트
             if turn < dp[ladder[idx]]:
                 dp[ladder[idx]] = turn
-                idx = ladder[idx] + 1
                 continue
+        # 사다리 도착점에 값이 없을 경우
         else:
             dp[ladder[idx]] = turn
-    # idx 칸이 사다리 시작점일 경우
+
+    # idx 칸이 뱀 시작점일 경우
     elif idx in snake_start:
+        # 이미 뱀 도착점에 값이 있을 경우
         if dp[snake[idx]]:
+            # 신규 값이 더 작을 경우 업데이트
             if turn < dp[snake[idx]]:
                 dp[snake[idx]] = turn
+                # 값이 업데이트되었기 때문에 해당 지점+1부터 다시 값 계산
                 idx = snake[idx] + 1
                 continue
+        # 뱀 도착점에 값이 없을 경우
         else:
             dp[snake[idx]] = turn
     else:
         dp[idx] = turn
     idx += 1
-print(dp)
+print(dp[-1])
