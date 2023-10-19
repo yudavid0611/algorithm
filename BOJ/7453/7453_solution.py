a523a98c51e39d16972add1413b780218f9efe8b
YU DAVID <yudavid0611@gmail.com>
2023-10-19 16:59:17 +0900

    boj 7453

--- BOJ/7453/7453_solution.py
+++ BOJ/7453/7453_solution.py
@@ -13,6 +13,7 @@ for _ in range(N):
     nums[2].append(c)
     nums[3].append(d)
 
+# key: A와 B의 합, value: 해당 key의 개수
 sum_ab = defaultdict(int)
 for i in nums[0]:
     for j in nums[1]:
@@ -23,6 +24,7 @@ answer = 0
 for i in nums[2]:
     for j in nums[3]:
         target = -(i + j)
+        # 0으로 만들 수 있는 수가 sum_ab에 있는지 확인
         if target in sum_ab:
             answer += sum_ab[target]
 
